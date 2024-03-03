7c211e1a091590201cab1562418142ea0a7f8127
Ransom070400 <148370144+Ransom070400@users.noreply.github.com>
2024-03-03 17:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,138 @@
+
 class Telephone {
+
     constructor() {
-      this._phoneNumbers = new Set();
-      this._observers = [];
+        if(Telephone.instance) {
+            return Telephone.instance;
+        } else {
+            this.contacts = new Set();
+            this.observers = [];
+            Telephone.instance = this;
+        }
     }
-  
+
     addPhoneNumber(phoneNumber) {
-      this._phoneNumbers.add(phoneNumber);
-      this.notifyObservers(phoneNumber, 'add');
+        if(this.contactExist(phoneNumber)) {
+            console.log("Contact already exist");
+        }else {
+            this.contacts.add(phoneNumber);
+        }
     }
-  
+
     removePhoneNumber(phoneNumber) {
-      this._phoneNumbers.delete(phoneNumber);
-      this.notifyObservers(phoneNumber, 'remove');
+        if(this.contactExist(phoneNumber)) {
+            this.contacts.delete(phoneNumber);
+        }else {
+            console.log("Contact doesn't exist");
+        }
     }
-  
+
     dialPhoneNumber(phoneNumber) {
-      if (this._phoneNumbers.has(phoneNumber)) {
-        this.notifyObservers(phoneNumber, 'dial');
-      } else {
-        console.log('Phone number not found');
-      }
+        if(this.contactExist(phoneNumber)) {
+            this.notifyObserver(phoneNumber);
+        }else {
+            console.log("Contact doesn't exist");
+        }
     }
-  
+
     addObserver(observer) {
-      this._observers.push(observer);
+        if(this.observerExist(observer)) {
+            console.log("Observer already exist");
+        }else {
+            this.observers.add(observer);
+        }
     }
-  
+
     removeObserver(observer) {
-      this._observers = this._observers.filter(o => o !== observer);
+        if(this.observerExist(observer)) {
+            this.observers.delete(observer);
+        }else {
+            console.log("Observer doesn't exist");
+        }
     }
-  
-    notifyObservers(phoneNumber, action) {
-      this._observers.forEach(observer => observer.notify(phoneNumber, action));
+
+    notifyObserver(phoneNumber) {
+        this.observers.forEach(observer => {
+            observer.update(phoneNumber);
+            console.log("");
+        });
     }
-  }
-  
-  class Observer {
-    constructor(callback) {
-      this._callback = callback;
+
+    contactExist(phoneNumber) {
+        return this.contacts.has(phoneNumber);
     }
-  
-    notify(phoneNumber, action) {
-      this._callback(phoneNumber, action);
+
+    observerExist(observer) {
+        return this.observers.has(observer);
     }
-  }
-  
-  const printPhoneNumber = (phoneNumber, action) => {
-    console.log(`Phone number: ${phoneNumber}`);
-  };
-  
-  const printDialing = (phoneNumber, action) => {
-    console.log(`Now Dialling ${phoneNumber}`);
-  };
-  
-  const telephone = new Telephone();
-  const printPhoneNumberObserver = new Observer(printPhoneNumber);
-  const printDialingObserver = new Observer(printDialing);
-  
-  telephone.addObserver(printPhoneNumberObserver);
-  telephone.addObserver(printDialingObserver);
-  
-  telephone.addPhoneNumber("2347023232");
-  telephone.dialPhoneNumber("2347023232");
-  telephone.removePhoneNumber("2347023232");
-  telephone.dialPhoneNumber("2347023232");
+
+}
+
+
+class Observer1 {
+    update(phoneNumber) {
+        console.log(Contact Details: Name: ${phoneNumber.fullName}\n\t\tNumber: ${phoneNumber.number})
+    }
+}
+
+
+class Observer2 {
+    update(phoneNumber) {
+        console.log(currently dialing......${phoneNumber.number});
+    }
+}
+
+
+const PhoneNumber = (firstName, lastName, number) => ({
+    fullName: ${firstName} ${lastName},
+    number
+});
+
+console.log("\nCreating contacts for ransom, dave and gift:");
+const ada = PhoneNumber("Ransom", "Eze", "+2347040060087");
+const jessie = PhoneNumber("Dave", "Eke", "+2348076543210");
+const nzube = PhoneNumber("Gift", "Okoro", "+2348076543210");
+console.log(ransom);
+console.log(dave);
+console.log(gift);
+
+
+console.log("\nCreating two Telephone objects and adding contacts and observers to just one but they both return the same thing.\n");
+const telephone1 = new Telephone();
+const telephone2 = new Telephone();
+
+const observer1 = new Observer1();
+const observer2 = new Observer2();
+
+telephone1.addPhoneNumber(ransom);
+telephone1.addPhoneNumber(dave);
+telephone1.addPhoneNumber(gift);
+
+telephone1.addObserver(observer1);
+telephone1.addObserver(observer2);
+
+console.log("Telephone1:");
+console.log(telephone1);
+console.log("\nTelephone2:");
+console.log(telephone2);
+
+console.log("\nCalling dave:");
+telephone1.dialPhoneNumber(dave);
+
+console.log("Calling gift:");
+telephone1.dialPhoneNumber(gift);
+
+console.log("\nDeleting ransom contact:");
+telephone1.removePhoneNumber(ransom);
+console.log(telephone1);
+
+/* Expaination
+The constructor ensures that only one instance of Telephone can exist. It initializes the contacts and observers arrays.
+addPhoneNumber, removePhoneNumber, and dialPhoneNumber methods are used to manage phone numbers in the contact list.
+The addObserver and removeObserver methods handle adding and removing observers who are interested in phone call events.
+The notifyObserver method notifies all observers when a phone call is made.
+There are two observer classes, Observer1 and Observer2, each with an update method to react to phone call events.
+The PhoneNumber function creates phone number objects with first name, last name, and number.
+The code then creates three phone number objects and two Telephone instances.
+It adds contacts and observers to one Telephone instance and demonstrates how both instances behave the same way.
+Finally, it makes phone calls, deletes a contact, and logs the phone call events.*/
